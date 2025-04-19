8aff39be11e6ec681cea9e17df5e6d3f8474e09d
jkalapati-ng <jayakrishna.alapati@gmail.com>
2025-04-19 12:44:34 +0530

    Add math operations module with add function

---
+++ src/calculator/math_operations.py
@@ -0,0 +1,11 @@
+def add(a: float, b: float) -> float:
+    """Add two numbers and return the result.
+
+    Args:
+        a (float): First number
+        b (float): Second number
+
+    Returns:
+        float: Sum of a and b
+    """
+    return a + b
